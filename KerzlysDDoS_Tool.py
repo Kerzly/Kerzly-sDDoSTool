1e25e2f38451edac74162bec8946bcff7d5111d8
Kerzly <ssafomemo@gmail.com>
2026-04-03 21:47:20 +0200

    Add files via upload

---
+++ KerzlysDDoS_Tool.py
@@ -0,0 +1,38 @@
+#A  open source DDoS used for educational purposes ONLY !
+#It is not Kerzly's fault if zou use this without Permission.
+#Coded bz Kerzly
+import time
+from colorama import Fore, Style, init
+init()
+print(Fore.RED + r" ____  __.                  .__         /\     ")
+print(r"|    |/ _|__________________|  | ___.__.)/_____")
+print(r"|      <_/ __ \_  __ \___   /  |<   |  |/  ___/")
+print(r"|    |  \  ___/|  | \//    /|  |_\___  |\___ \ ")
+print(r"|____|__ \___  >__|  /_____ \____/ ____/____  >")
+print(r"        \/   \/            \/    \/         \/  ")
+print(r"________  ________          _________ ___________           .__  ")
+print(r"\______ \ \______ \   ____ /   _____/ \__    ___/___   ____ |  | ")
+print(r" |    |  \ |    |  \ /  _ \\_____  \    |    | /  _ \ /  _ \|  |")
+print(r" |    `   \|    `   (  <_> )        \   |    |(  <_> |  <_> )  |__")
+print(r"/_______  /_______  /\____/_______  /   |____| \____/ \____/|____/")
+print(r"        \/        \/              \/                         ")
+print()
+print(Style.RESET_ALL)
+print()
+input(Fore.BLUE + "Enter Target Ip:")
+time.sleep(4.2)
+print()
+print("Host found !")
+print()
+print()
+time.sleep(6)
+nam: int = 0
+
+while nam < 10000000:
+    nam += 1
+    print(f"Amount of 64bytes Packages sent: {nam}")
+
+
+
+
+
